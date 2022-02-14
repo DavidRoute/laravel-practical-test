dcd4cd0acdfc05b8e0001a2c90d8449f85c1f8ba
Myat Thu <mr.myatthu027@gmail.com>
2022-02-14 12:09:15 +0630

    Register and login API

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    /**
+     * API Register
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request) 
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors()], 422);
+        }
+
+        $data = $validator->validate();
+        $data['password'] = bcrypt($data['password']);
+
+        $user = User::create($data);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    /**
+     * API Login
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request) 
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+ 
+        if (! $user || ! \Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'The provided credentials are incorrect.']);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+}
